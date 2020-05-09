81925ef1ddfca398b29865f6f388b0bf72a0e0bb
digitbase <digitbase@gmail.com>
2020-05-09 18:57:47 +0800

    ignor

--- myword/src/js/index.js
+++ myword/src/js/index.js
@@ -1,2 +1,2 @@
-console.log('1xx1')
+console.log('1x21')
 
--- myword/webpack.product.config.js
+++ myword/webpack.product.config.js
@@ -13,7 +13,7 @@ module.exports={
 	},
 	output:{
 		path: path.resolve(__dirname+'/dist/js'),
-		filename: '[name]-[hash].js'
+		filename: '[name].js'
 	},
 	module:{
 		rules:[
