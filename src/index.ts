8148ee864a75b43151aee43f46539a1dcb2dd064
digitbase <digitbase@gmail.com>
2022-11-12 23:53:11 +0800

    调试环境

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
 
 console.log('index');
+
+function pr(input:any, type2?:number):void{
+	console.log(typeof input, "==>",input);let _type = type2?type2:0;let str = ""
+	if (_type > 0) {for(let i=0; i < 20; i++)  str +=_type.toString();console.log(`==============    ${str}    ===============`)}
+}
+
+
+
+
+function buildLabel(name: string): string {
+  return buildLabel.prefix + name + buildLabel.suffix; //可访问合并中的命名空间中的属性
+}
+
+namespace buildLabel {
+  export let suffix = "";
+  export let prefix = "Hello, ";
+}
+
+
+pr(buildLabel("Sam Smith"));
+
+pr('test1')
