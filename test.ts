d26fd40db2cb654408bc547fc2efd66eedf79528
digitbase <digitbase@gmail.com>
2022-11-12 22:54:51 +0800

    ts init

---
+++ test.ts
@@ -0,0 +1,43 @@
+
+function pr(input:any, type2?:number):void{
+	console.log(typeof input, "==>",input);let _type = type2?type2:0;let str = ""
+	if (_type > 0) {for(let i=0; i < 20; i++)  str +=_type.toString();console.log(`==============    ${str}    ===============`)}
+}
+
+
+
+
+function buildLabel(name: string): string {
+  return buildLabel.prefix + name + buildLabel.suffix; //可访问合并中的命名空间中的属性
+}
+
+namespace buildLabel {
+  export let suffix = "";
+  export let prefix = "Hello, ";
+}
+
+
+pr(buildLabel("Sam Smith"));
+
+
+interface Person {
+	name : string
+}
+
+
+export class MyObj<t>{
+	t1 = function(){
+		pr("t1 function")
+	}
+}
+
+declare global {
+	interface Person<t> {
+		t2() : MyObj<t>;
+	}
+}
+
+
+
+console.log(typeof MyObj)
+
